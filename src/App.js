f57a13ece1980ae83e443cce3745f19df66850d8
Manish Raj <42971673+manishrajmishra@users.noreply.github.com>
2020-12-26 20:45:43 +0530

    feat: sending message and storing the messages

--- src/App.js
+++ src/App.js
@@ -1,11 +1,41 @@
-import React from "react";
+import React, { useState } from "react";
+import { Button, FormControl, Input, InputLabel } from "@material-ui/core";
 import "./styles.css";
 
 export default function App() {
+  const [input, setInput] = useState("");
+  const [messages, setMessages] = useState([]);
+
+  const sendMessage = (event) => {
+    //all the logic to send a message goes here
+    event.preventDeafault();
+    setMessages([...messages, input]);
+    setInput("");
+  };
+
   return (
     <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
+      <form>
+        <FormControl>
+          <InputLabel>Enter the messages....</InputLabel>
+          <Input
+            value={input}
+            onChange={(event) => setInput(event.target.value)}
+          />
+          <Button
+            disabled={!input}
+            variant="contained"
+            color="primary"
+            type="submit"
+            onClick={sendMessage}
+          >
+            Send Message
+          </Button>
+        </FormControl>
+      </form>
+      {messages.map((message) => (
+        <p>{message}</p>
+      ))}
     </div>
   );
 }
